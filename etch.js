80b8e3751a7c78a944403751f41a9683cbee53f1
montebruce <montebruce@proton.me>
2025-04-18 10:26:13 -0500

    Finished the project with one added extra credit.

--- etch.js
+++ etch.js
@@ -2,27 +2,36 @@ const button = document.getElementById("generate");
 const mainContainer = document.getElementById("container");
 
 button.addEventListener("click", function(){
-    const rowcount = parseInt(document.getElementById("row-input").value, 10); 
+    const rowcount = parseInt(document.getElementById("row-input").value); 
 
-    if (isNaN(rowcount) || rowcount<1 || rowcount>100){
-        alert("Please enter a whole number between and 100");
+    function getRandomHexColor() {
+        const hex = Math.floor(Math.random() * 0xffffff).toString(16);
+        return `#${hex.padStart(6, '0')}`;
+    }
+
+    if (isNaN(rowcount) || rowcount<1 || rowcount>200){
+        alert("Please enter a whole number between 1 and 200 ");
         return;
     }
 
     mainContainer.innerHTML = ""
+    
+    const rowheight = 100/rowcount;
+    const totalbox = rowcount*rowcount
+    
 
-    let counter=1;
+    for(let r = 0; r<totalbox; r++){
+        const box = document.createElement("div");
+        box.className = "box";
+        box.style.backgroundColor = getRandomHexColor();
+        box.style.height = `${rowheight}%`;
+        box.style.width = `${rowheight}%`;
+        box.innerHTML = "&nbsp;";
+        box.addEventListener("mouseenter", function() {
+            box.style.backgroundColor = "black";
+            })
+        mainContainer.appendChild(box);
+        
+    }
 
-    for(let r = 0; r<rowcount; r++){
-        const row = document.createElement("div");
-        row.classname="row";
-
-        for(let c = 0; c<rowcount; c++){
-            const box = document.createElement("div")
-            div.className = "box";
-            box.textContent = counter++
-            row.appendChild(box);
-        }
-    }mainContainer.appendChild(row);
-
-})
+}) 
